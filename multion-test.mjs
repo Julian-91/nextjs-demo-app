e6a2298a5d185bc3afbbbd0e2437ebfb5ae9d16b
Julian Boot <julian_boot@hotmail.com>
2024-12-20 13:40:36 +0100

    Chang to script

--- multion-test.mjs
+++ multion-test.mjs
@@ -7,6 +7,7 @@ const prUrl = process.argv[2];
 const previewUrl = process.argv[3];
 
 const multion = new MultiOnClient({ apiKey: process.env.MULTION_API_KEY });
+
 const prResponse = await multion.browse({
     cmd: 'Based on the content of the PR, output a clear test to perform on the preview site.',
     url: prUrl,
@@ -19,7 +20,7 @@ console.log("### PR analysis:\n" + prResponse.message);
 const previewResponse = await multion.browse({
     cmd: "Output the results of the performed test: " + prResponse.message,
     url: previewUrl,
-    maxSteps: 3,
+    maxSteps: 5,
 });
 
 // Set output for GitHub Actions
